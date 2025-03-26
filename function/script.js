e76e3a21e703479f4b8dcd512101781b2fe7f3a8
shahzad11khan <zarakali401@gmail.com>
2025-03-26 15:12:47 +0500

    add js file

---
+++ function/script.js
@@ -0,0 +1,104 @@
+// console.log("hello, javascript🌹");
+
+// variable types
+// let , var , const
+
+// hoisting
+
+// console.log("my variable is: ",a);
+// var a = 20 ; //out put undefined
+// let a = 20; 
+
+// let name ="javascript"
+
+// alert(`hello, ${name}🌹`);
+// alert("hello",name, "🌹");
+
+// to show the message from the side of prog
+// document.write("hello javascript 🌹")
+
+// to take input from the user
+
+// prompt("Enter your name :")
+
+// let _name = "ali"
+// let name = "ali"
+// let _name = true;
+// console.log(`my cnic num is : ${_name} & my type of is ${typeof(_name)}`);
+
+// non-primitive 
+
+// let name= "Ali"; //primitive
+// let object = {
+//     // key : value
+//     name:"khan",
+//     age:21,
+// }
+
+// console.log(object);
+
+// const webclass = [
+//     {
+//         name: "khan",
+//         age: 22,
+//     },
+//     {
+//         name: "Ali",
+//         age: 23,
+//     },
+//     {
+//         name: "Husain",
+//         age: 23,
+//     }
+// ]
+
+// console.log(webclass);
+
+// const arr = ["table","width",10,undefined,null]
+
+// console.log(arr)
+
+// prompt("enter you age:") 
+
+// conditional statements
+
+// if 
+// if-else
+// else-if
+// switch
+
+let marks= prompt("Enter marks:");
+console.log(marks, typeof(marks))
+
+// 21
+if(isNaN(marks)){
+    console.log("Your Enter String",marks)
+}else{
+    console.log("Your Enter Number",marks);
+}
+
+// just if
+// if(marks>90){
+//     console.log("A grade");
+// }
+
+// if else
+
+// if(marks > 90){
+// body of if
+// }else{
+// body of else
+// }
+
+// else if 
+
+// if(marks>90){
+// console.log("Grade A")
+// }else if(marks >= 80 && marks > 10){
+// console.log("Grade B")
+// }else{
+//    console.log("Grade C") 
+// }
+
+// switch
+
