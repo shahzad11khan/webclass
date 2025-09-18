6d7ae7f57d98b39c6add084b7f2ddb259306ff47
shahzad11khan <zarakali401@gmail.com>
2025-09-18 14:16:49 +0500

    today work on variables and reservewords and keyword

--- script/script.js
+++ script/script.js
@@ -1,3 +1,26 @@
 // when u use console on front-end the result is show on the browser console 
 // when u use console on back-end the result is show on the vs code terminal or on server ternimal
-console.log("Hello, Javascript🌹");
+// console.log("Hello, Javascript🌹");
+// alert("lorem");
+// document.write("Hello, Javascript");
+
+
+// var  --> shortcut from of variable  --> actual name is keyword reserveword --> which is used for declear a variable
+// keyword/resverword variableName = value/data
+
+// var batchII = "javascript";
+// console.log(batchII);
+// document.write(batchII)
+// alert(batchII)
+// var batchI = "students"
+// var advanceweb = "students"
+// var MyName = "John Doe";
+// var myname= "Jane Doe";
+// let MyName = "John Doe";
+// let MyName = "shahzad";
+const MyName = "shahzad";
+var myName = "John Doe";
+let MYName = "Jane Doe";
+// const MyName = "John Doe"; 
+document.writeln("<h1>" + MyName + "</h1>");
+
