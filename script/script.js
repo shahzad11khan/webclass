6fc673c076c3f47cdffcffca0ed24ccec5219ca3
shahzad11khan <zarakali401@gmail.com>
2025-09-18 11:26:21 +0500

    study console mean print result

---
+++ script/script.js
@@ -0,0 +1,3 @@
+// when u use console on front-end the result is show on the browser console 
+// when u use console on back-end the result is show on the vs code terminal or on server ternimal
+console.log("Hello, Javascript🌹");
