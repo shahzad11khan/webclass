c924194d4084e84f24223e9fa7ae514f8a7b68fe
shahzad11khan <zarakali401@gmail.com>
2025-09-19 14:38:33 +0500

    primitive and its types

--- script/script.js
+++ script/script.js
@@ -1,10 +1,11 @@
+// class 01
 // when u use console on front-end the result is show on the browser console 
 // when u use console on back-end the result is show on the vs code terminal or on server ternimal
 // console.log("Hello, Javascript🌹");
 // alert("lorem");
 // document.write("Hello, Javascript");
 
-
+// class 02
 // var  --> shortcut from of variable 
 // var --> actual name is keyword reserveword --> which is used for declear a variable
 // keyword/resverword     variableName      = value/data
@@ -19,10 +20,58 @@
 // let MyName = "John Doe";
 
 // let MyName = "shahzad";
-const MyName = "shahzad";
-var myName = "John Doe";
-console.log(MyName);
+// const MyName = "shahzad";
+// var myName = "John Doe";
+// console.log(MyName);
 // let MYName = "Jane Doe";
 // const MyName = "John Doe"; 
 // document.writeln("<h1>" + MyName + "</h1>");
+
+// class 03
+// let MyName = "shahzad";
+// let age = 25;
+// let isStudent = true;
+// let Mychar = 'A';
+// let MyName = "John Doe";
+// Declaration of variable
+// use for future use
+// let MyName;
+// let age;
+// let isStudent;
+// initilaization of variable
+// MyName = "shahzad";
+// age = 25;
+// isStudent = true;
+// declaration and initialization of variable
+// use for code optimization
+// let Mychar = 'A';
+// let code = 34;
+// type of datatype/variablesname
+// primitive datatype
+// string
+// number
+// boolean
+// char
+// undefined
+// null
+
+let MyName = "shahzad";
+let age = 25; // datatype number , primitive datatype
+let isStudent = true; // datatype boolean , primitive datatype
+let flotvalue = 34.56;
+let arr = [1,2,3,4,5];// non-primitive datatype object
+// console.log(typeof arr); 
+let obj = {name: "shahzad", age: 25}; //object non-primitive datatype
+let undef = undefined; // datatype undefined , primitive datatype 
+// console.log(typeof undef);
+let n = null;
+//  n = "string"; // datatype null , non-primitive datatype
+// n={name: "shahzad", age: 25}; //object non-primitive datatype
+// console.log(n); // object non-primitive datatype
+// let x;
+// console.log(x);
+// console.log(typeof MyName);
+// console.log(typeof age);
+// console.log(typeof flotvalue);
+
 
